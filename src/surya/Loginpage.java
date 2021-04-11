0faec5479f460c814a1ae7b4c76eced50cf1e589
kiran <kiran@192.168.1.7>
2021-04-11 12:59:10 +0530

    surya changes websit package deleted

--- src/surya/Loginpage.java
+++ src/surya/Loginpage.java
@@ -1,24 +1,33 @@
 package surya;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Loginpage {
 	String title = "FACEBOOK LOGIN", uname, pword, name, luname, lpword;
-	Scanner scan = new Scanner(System.in);
 
 	void printBaseMenu() {
-		System.out.println(title);
+		System.out.println("\t\t\t\t" + title);
 		System.out.println("1.REGISTER:");
 		System.out.println("2.LOGIN");
 		System.out.println("Enter your choice:");
-		int choice = scan.nextInt();
 
-		if (choice == 1)
-			printRegMenu();
-		else if (choice == 2)
-			printLogin();
+		try {
+			Scanner scan = new Scanner(System.in);
+			int choice = scan.nextInt();
+			if (choice == 1)
+				printRegMenu();
+			else if (choice == 2)
+				printLogin(false);
+		} catch (InputMismatchException e) {
+			System.out.println("please enter number only");
+			printBaseMenu();
+		}
+
 	}
 
 	void printRegMenu() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter username: (NOTE:characters should be between 4-12digits)");
 		uname = scan.next();
 
@@ -31,7 +40,8 @@ public class Loginpage {
 
 	}
 
-	 void getRegisterPassword() {
+	void getRegisterPassword() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter password: (NOTE:characters should be between 6-18digits)");
 		pword = scan.next();
 
@@ -40,7 +50,7 @@ public class Loginpage {
 			System.out.println("Enter your name:");
 			name = scan.next();
 
-			printLogin();
+			printLogin(false);
 		} else {
 			System.out.println("invalid password,plz try again");
 			getRegisterPassword();
@@ -53,37 +63,44 @@ public class Loginpage {
 		ss.printBaseMenu();
 	}
 
-	void printLogin() {
+	void printLogin(boolean isChange) {
+		Scanner scan = new Scanner(System.in);
 
 		System.out.println("Enter loginusername:");
 		luname = scan.next();
 		System.out.println("Enter loginpassword:");
 		lpword = scan.next();
 		if (luname.equalsIgnoreCase(uname) && (lpword.equals(pword))) {
-			dashboard();
+			dashboard(isChange);
 		} else {
 			System.out.println("username(or)password doesnt match");
-			printLogin();
+			printLogin(false);
 		}
 
 	}
 
-	void dashboard() {
-		System.out.println("hello" + name);
-		System.out.println("1.Logout");
-		System.out.println("2.Change username");
-		System.out.println("3.Change password");
-		System.out.println("Enter your option:");
-		int option = scan.nextInt();
-		if (option == 1) {
-			printLogoutMenu();
-		} else if (option == 2) {
-			changeNewUserName();
-		} else if (option == 3) {
-			changeNewPassword();
+	void dashboard(boolean isChange) {
+
+		if (isChange) {
+			System.out.println("ok");
 		} else {
-			System.out.println("incorrect option");
-			dashboard();
+			Scanner scan = new Scanner(System.in);
+			System.out.println("hello " + name);
+			System.out.println("1.Logout");
+			System.out.println("2.Change username");
+			System.out.println("3.Change password");
+			System.out.println("Enter your option:");
+			int option = scan.nextInt();
+			if (option == 1) {
+				printLogoutMenu();
+			} else if (option == 2) {
+				changeNewUserName();
+			} else if (option == 3) {
+				changeNewPassword();
+			} else {
+				System.out.println("incorrect option");
+				dashboard(isChange);
+			}
 		}
 	}
 
@@ -92,23 +109,26 @@ public class Loginpage {
 	}
 
 	void changeNewUserName() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter username: (NOTE:characters should be between 4-12digits)");
 		uname = scan.next();
 
 		if (uname.length() >= 4 && uname.length() <= 12) {
-			printLogin();
+			printLogin(true);
 		} else {
 			System.out.println("invalid username, plz try again");
 			printRegMenu();
 		}
 
 	}
+
 	void changeNewPassword() {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter password: (NOTE:characters should be between 6-18digits)");
 		pword = scan.next();
 
 		if (pword.length() >= 6 && pword.length() <= 18) {
-			printLogin();
+			printLogin(true);
 		} else {
 			System.out.println("invalid password,plz try again");
 			getRegisterPassword();
